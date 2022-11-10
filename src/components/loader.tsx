bc3d8fa0d9b7f41f186dd70fa7ebf403e6756eb6
tossode <tossdev@189.cn>
2022-11-10 21:26:47 +0800

    tosslate created a commit :sparkles:

--- src/components/loader.tsx
+++ src/components/loader.tsx
@@ -3,9 +3,15 @@ import css from '../styles/scoped/loader.module.css'
 export default function Loader() {
   return (
     <ul className="flex justify-center list-none space-x-1">
-      <li className={`bg-zinc-400 opacity-0 rounded w-2 h-2 ${css['dots-1']}`}></li>
-      <li className={`bg-zinc-400 opacity-0 rounded w-2 h-2 ${css['dots-2']}`}></li>
-      <li className={`bg-zinc-400 opacity-0 rounded w-2 h-2 ${css['dots-3']}`}></li>
+      <li
+        className={`bg-zinc-400 opacity-0 rounded w-2 h-2 ${css['dots-1']}`}
+      ></li>
+      <li
+        className={`bg-zinc-400 opacity-0 rounded w-2 h-2 ${css['dots-2']}`}
+      ></li>
+      <li
+        className={`bg-zinc-400 opacity-0 rounded w-2 h-2 ${css['dots-3']}`}
+      ></li>
     </ul>
   )
 }
