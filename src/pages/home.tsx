57e5efeaaf6bb68a5ca70b07336f80da44134afe
tossode <tossdev@189.cn>
2022-11-11 23:39:38 +0800

    tosslate created a commit :sparkles:

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -23,7 +23,7 @@ export default function HomePage() {
             <Loader />
           </Then>
           <Else>
-            <div className="flex items-center divide-x divide-zinc-500">
+            <div className="flex items-center divide-x divide-zinc-500 p-6">
               <h3 className="text-2xl text-zinc-700 pr-4">👋</h3>
               <p className="text-zinc-700 pl-4 py-2">
                 {data?.content[random(data.length - 1)]}
