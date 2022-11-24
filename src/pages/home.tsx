c34af1237ebb09a9f9600fd01aa5db354f107823
tossode <tossdev@189.cn>
2022-11-24 20:38:28 +0800

    tosslate created a commit :sparkles:

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -12,7 +12,7 @@ async function findFirst() {
 }
 
 export default function HomePage() {
-  const title = 'shih-ching'
+  const title = '随机诗词'
   const { isLoading, data } = useQuery('shih-ching', () => findFirst())
 
   return (
@@ -25,8 +25,8 @@ export default function HomePage() {
           <Else>
             <div className="flex items-center divide-x divide-zinc-300 p-6">
               <h3 className="text-2xl text-zinc-600 pr-4">👋</h3>
-              <p className="text-zinc-600 leading-relaxed pl-4 py-2">
-                <span className="duration-200 ease-in-out transition-[background-size] bg-gradient-to-r from-sky-300 to-lime-300 bg-left-bottom bg-no-repeat bg-[length:0px_2px] hover:bg-[length:100%_2px]">
+              <p className="text-zinc-600 leading-loose pl-4 py-2">
+                <span className="duration-300 ease-in-out transition-[background-size] bg-gradient-to-r from-sky-300 to-lime-300 bg-left-bottom bg-no-repeat bg-[length:0px_2px] hover:bg-[length:100%_2px] py-1">
                   {data?.content[random(data.length - 1)]}
                 </span>
               </p>
